3f918772cf78759b64472ca14a7c3b5a3447c432
Jason Zaugg <jzaugg@gmail.com>
2016-09-02 14:37:48 +1000

    More structure and benchmarks

--- build.sbt
+++ build.sbt
@@ -1,14 +1,35 @@
-name := """sbt-jmh-seed"""
+name := """scala-jmh-suite"""
 
 version := "1.0-SNAPSHOT"
 
 scalaVersion in ThisBuild := "2.11.8"
 
-val compilerMacroBench = project.enablePlugins(JmhPlugin).settings(
+// Convenient access to builds from PR validation
+resolvers ++= (
+  if (scalaVersion.value.endsWith("-SNAPSHOT"))
+    List(
+      "pr-scala snapshots old" at "http://private-repo.typesafe.com/typesafe/scala-pr-validation-snapshots/",
+      "pr-scala snapshots" at "https://scala-ci.typesafe.com/artifactory/scala-pr-validation-snapshots/",
+      Resolver.mavenLocal,
+      Resolver.sonatypeRepo("snapshots")
+    )
+  else
+    Nil
+)
+
+val compilation = project.enablePlugins(JmhPlugin).settings(
+  // We should be able to switch this project to a broad range of Scala versions for comparative
+  // benchmarking. As such, this project should only depend on the high level `MainClass` compiler API.
+  description := "Black box benchmark of the compiler",
   libraryDependencies += "org.scala-lang" % "scala-compiler" % scalaVersion.value
 )
 
-//javaOptions in (Jmh, run) in ThisBuild := {
-//  import scala.collection.JavaConverters._
-//  java.lang.management.ManagementFactory.getRuntimeMXBean.getInputArguments.asScala
-//}
+val micro = project.enablePlugins(JmhPlugin).settings(
+  description := "Finer grained benchmarks of compiler internals",
+  libraryDependencies += "org.scala-lang" % "scala-compiler" % scalaVersion.value
+)
+
+val jvm = project.enablePlugins(JmhPlugin).settings(
+  description := "Pure Java benchmarks for demonstrating performance anomolies independent from the Scala language/library",
+  autoScalaLibrary := false
+)
---
+++ compilation/src/main/scala/scala/tools/nsc/ScalacBenchmark.scala
@@ -0,0 +1,93 @@
+package scala.tools.nsc
+
+import java.io.File
+import java.nio.file.{Files, Path, Paths}
+import java.util.concurrent.TimeUnit
+import java.util.stream.Collectors
+
+import org.openjdk.jmh.annotations._
+import org.openjdk.jmh.annotations.Mode._
+
+@State(Scope.Benchmark)
+class ScalacBenchmark {
+  @Param(value = Array[String]())
+  var source: String = _
+  @Param(value = Array(""))
+  var extraArgs: String = _
+  var driver: MainClass = _
+
+  protected def compile(): Unit = {
+     var compilerArgs =
+      if (source.startsWith("@")) Array(source)
+      else {
+        import scala.collection.JavaConverters._
+        val allFiles = Files.walk(findSourceDir).collect(Collectors.toList[Path]).asScala.toList
+        allFiles.filter(_.getFileName.toString.endsWith(".scala")).map(_.toAbsolutePath.toString).toArray
+      }
+    compilerArgs ++= extraArgs.split("""\s+""")
+    val driver = new scala.tools.nsc.MainClass {
+      override protected def processSettingsHook(): Boolean = {
+        settings.usejavacp.value = true
+        settings.outdir.value = tempOutDir.getAbsolutePath
+        settings.nowarn.value = true
+        if (extraArgs != null && extraArgs != "")
+          settings.stopAfter.value = extraArgs :: Nil
+        true
+      }
+    }
+    driver.process(compilerArgs)
+    assert(!driver.reporter.hasErrors)
+  }
+
+  private def tempOutDir: File = {
+    val tempFile = java.io.File.createTempFile("output", "")
+    tempFile.delete()
+    tempFile.mkdir()
+    tempFile
+  }
+  private def findSourceDir: Path = {
+    val path = Paths.get("../corpus/" + source)
+    if (Files.exists(path)) path
+    else Paths.get(source)
+  }
+}
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(SingleShotTime))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+// TODO -Xbatch reduces fork-to-fork variance, but incurs 5s -> 30s slowdown
+@Fork(value = 20, jvmArgs = Array("-XX:CICompilerCount=2"))
+class ColdScalacBenchmark extends ScalacBenchmark {
+  @Benchmark
+  override def compile(): Unit = super.compile()
+}
+
+@BenchmarkMode(Array(AverageTime))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Measurement(iterations = 1, time = 30, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 20)
+class WarmScalacBenchmark extends ScalacBenchmark {
+  @Benchmark
+  override def compile(): Unit = super.compile()
+}
+
+@BenchmarkMode(Array(SampleTime))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 6, time = 10, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 12, time = 10, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 5)
+class HotScalacBenchmark extends ScalacBenchmark {
+  @Benchmark
+  override def compile(): Unit = super.compile()
+}
+
+@BenchmarkMode(Array(SampleTime))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 6, time = 10, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 12, time = 10, timeUnit = TimeUnit.SECONDS)
+@Threads(Threads.MAX)
+@Fork(value = 5)
+class HotConcurrentScalacBenchmark extends ScalacBenchmark {
+  @Benchmark
+  override def compile(): Unit = super.compile()
+}
