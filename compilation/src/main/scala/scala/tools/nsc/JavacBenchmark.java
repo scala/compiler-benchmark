05d88eb319fa97746ed692b7dd40e92765965825
Jason Zaugg <jzaugg@gmail.com>
2017-11-29 11:12:19 +1000

    Add a comparative Java vs Scala benchmark
    9kloc of Java code faithfully transliterated to Scala by the
    good folks at Scala Native and put to use in twitter/reasonable-scala
    as a comparative benchmark.
    
    I've fleshed out the Javac benchmark driver to let us
    run this.
    
    ```
    > compilation/jmh:run scala.tools.nsc.JavacBenchmark -psource=re2j
    
    > hot -psource=re2s
    ```
    
    On my machine, scalac gets to 7500 loc/s. Javac hits a whopping 130kloc/s.

--- compilation/src/main/scala/scala/tools/nsc/JavacBenchmark.java
+++ compilation/src/main/scala/scala/tools/nsc/JavacBenchmark.java
@@ -1,37 +1,104 @@
 package scala.tools.nsc;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import org.openjdk.jmh.annotations.*;
+
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
+import static org.openjdk.jmh.annotations.Mode.SampleTime;
 
+@BenchmarkMode(SampleTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 10, time = 10, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 10, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 3, jvmArgs = {"-Xms2G", "-Xmx2G"})
 @State(Scope.Thread)
 public class JavacBenchmark {
-    // Prepare source somehow.
-    String source = "public class Test { public Test() { System.out.println(\"world\"); } }";
+    @Param(value = {})
+    String source;
+
+    // This parameter is set by ScalacBenchmarkRunner / UploadingRunner based on the Scala version.
+    // When running the benchmark directly the "latest" symlink is used.
+    @Param(value = {"latest"})
+    String corpusVersion;
+
     private JavaCompiler compiler;
     private Path path;
 
+    private Path corpusSourcePath() {
+        return Paths.get("..", "corpus", source, corpusVersion); };
+
+    private Path findSourceDir() {
+        Path path = corpusSourcePath();
+        return Files.exists(path) ? path : Paths.get(source);
+    }
+
+    private List<String> sourceFiles() {
+        if (source.startsWith("@")) return Collections.emptyList();
+        else {
+            List<Path> allFiles = walk(findSourceDir()).collect(Collectors.<Path>toList());
+            return allFiles.stream().filter(f -> {
+                String name = f.getFileName().toString();
+                return name.endsWith(".scala") || name.endsWith(".java");
+            }).map(x -> x.toAbsolutePath().normalize().toString()).collect(Collectors.toList());
+        }
+    }
+
+    private Stream<Path> walk(Path dir)  {
+        try {
+            return Files.walk(dir, FileVisitOption.FOLLOW_LINKS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Setup
     public void setup() throws IOException {
-        Path temp = Files.createTempDirectory("JavacBenchmark");
-        path = temp.resolve("Test.java");
         compiler = ToolProvider.getSystemJavaCompiler();
-        Files.write(path, source.getBytes(Charset.forName("UTF-8")));
     }
 
     @Benchmark
     public int bench() {
         // Compile source file.
-        int run = compiler.run(null, null, null, path.toAbsolutePath().toString());
+        List<String> args = new ArrayList();
+        args.add("-d");
+        args.add(tempDir.getAbsolutePath());
+        args.addAll(sourceFiles());
+        int run = compiler.run(null, null, null, args.toArray(new String[]{}));
         assert run == 0;
         return run;
     }
+
+
+    private File tempDir = null;
+
+    // Executed once per fork
+    @Setup(Level.Trial) public void initTemp() {
+        File tempFile = null;
+        try {
+            tempFile = File.createTempFile("output", "");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        tempFile.delete();
+        tempFile.mkdir();
+        tempDir = tempFile;
+    }
+    @TearDown(Level.Trial) public void clearTemp() {
+        BenchmarkUtils.deleteRecursive(tempDir.toPath());
+    }
+
 }
