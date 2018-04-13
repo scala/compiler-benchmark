2aa7450df7fa718789ed05437c6e7e61699d99a1
Lukas Rytz <lukas.rytz@gmail.com>
2018-04-13 22:38:53 +0200

    New version of the `vector` corpus compatible with the 2.13 collections
    Not updating the `latest` symlink for now.

--- infrastructure/src/main/java/scala/bench/ScalacBenchmarkRunner.java
+++ infrastructure/src/main/java/scala/bench/ScalacBenchmarkRunner.java
@@ -26,6 +26,8 @@ public class ScalacBenchmarkRunner {
 
             switch (source) {
                 case "vector":
+                    if (GitWalker.isAncestor("6ff3fac", scalaRef, repo)) // 2.13 collections were merged in 6ff3fac
+                        return "e3df10a";
                     if (GitWalker.isAncestor("v2.11.0", scalaRef, repo))
                         return "fb04376"; // compiles with 2.11.0, but not with 2.10.6
                     break;
