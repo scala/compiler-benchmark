094384dea68058baa3e5b467e7fc4d932e437a68
Jason Zaugg <jzaugg@gmail.com>
2019-02-05 10:08:40 +1000

    Move <:< and =:= to top-level
    Partial cherry pick of https://github.com/scala/scala/commit/df29ebbd91481a8468dde2b06a96438291824f73

--- corpus/scala/df29ebb/library/scala/Predef.scala
+++ corpus/scala/df29ebb/library/scala/Predef.scala
@@ -475,50 +475,10 @@ object Predef extends LowPriorityImplicits with DeprecatedPredef {
   /** @group conversions-java-to-anyval */
   implicit def Boolean2boolean(x: java.lang.Boolean): Boolean = x.asInstanceOf[Boolean]
 
-  // Type Constraints --------------------------------------------------------------
-
-  /**
-   * An instance of `A <:< B` witnesses that `A` is a subtype of `B`.
-   * Requiring an implicit argument of the type `A <:< B` encodes
-   * the generalized constraint `A <: B`.
-   *
-   * @note we need a new type constructor `<:<` and evidence `conforms`,
-   * as reusing `Function1` and `identity` leads to ambiguities in
-   * case of type errors (`any2stringadd` is inferred)
-   *
-   * To constrain any abstract type T that's in scope in a method's
-   * argument list (not just the method's own type parameters) simply
-   * add an implicit argument of type `T <:< U`, where `U` is the required
-   * upper bound; or for lower-bounds, use: `L <:< T`, where `L` is the
-   * required lower bound.
-   *
-   * In part contributed by Jason Zaugg.
-   * @group type-constraints
-   */
-  @implicitNotFound(msg = "Cannot prove that ${From} <:< ${To}.")
-  sealed abstract class <:<[-From, +To] extends (From => To) with Serializable
-  private[this] final val singleton_<:< = new <:<[Any,Any] { def apply(x: Any): Any = x }
-  // The dollar prefix is to dodge accidental shadowing of this method
-  // by a user-defined method of the same name (SI-7788).
-  // The collections rely on this method.
-  /** @group type-constraints */
-  implicit def $conforms[A]: A <:< A = singleton_<:<.asInstanceOf[A <:< A]
+  implicit def $conforms[A]: A <:< A = <:<.asInstanceOf[A <:< A]
 
   @deprecated("use `implicitly[T <:< U]` or `identity` instead.", "2.11.0")
   def conforms[A]: A <:< A = $conforms[A]
-
-  /** An instance of `A =:= B` witnesses that the types `A` and `B` are equal.
-   *
-   * @see `<:<` for expressing subtyping constraints
-   * @group type-constraints
-   */
-  @implicitNotFound(msg = "Cannot prove that ${From} =:= ${To}.")
-  sealed abstract class =:=[From, To] extends (From => To) with Serializable
-  private[this] final val singleton_=:= = new =:=[Any,Any] { def apply(x: Any): Any = x }
-  /** @group type-constraints */
-  object =:= {
-     implicit def tpEquals[A]: A =:= A = singleton_=:=.asInstanceOf[A =:= A]
-  }
 
   /** A type for which there is always an implicit value.
    *  @see [[scala.Array$]], method `fallbackCanBuildFrom`
